6d8ba3c8764dfc56e6e870ebeb14fb8137e38486
Turbo Gordz <gordon.draine97@yahoo.com>
2022-08-25 02:34:57 -0400

    creating navbar almsot complete mobile view

--- protfolio-gordon/src/App.js
+++ protfolio-gordon/src/App.js
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-brandcard">Hello world</h1>
+      <Navbar />
     </div>
   );
 }
